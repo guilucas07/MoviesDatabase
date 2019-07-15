83387e8ae6c2711bd02192ea95f4e739136a4b09
Guilherme Lucas <guilucas07@MacBook-Pro.local>
2019-07-15 20:56:51 -0300

    remove useless class

--- presentation/src/main/java/com/guilhermelucas/moviedatabase/model/RequestFailure.kt
+++
@@ -1,7 +0,0 @@
-package com.guilhermelucas.moviedatabase.model
-
-sealed class RequestFailure(val errorMessage: String) {
-    class NetworkConnection : RequestFailure("Verify your internet connection and try again!")
-    class ItemNotFound : RequestFailure("Item not found on database")
-    class GenericFailure(errorMessage: String) : RequestFailure(errorMessage)
-}
